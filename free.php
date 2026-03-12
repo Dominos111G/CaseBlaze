dba4d983bb239ca9b676c3a91c0249cb4bcb39fe
Dominik Szczerbal <domin.szczerbal@wp.pl>
2026-03-12 21:21:03 +0100

    Wyświetlanie skrzynek

--- free.php
+++ free.php
@@ -11,5 +11,33 @@
 <body>
     <?php include 'includes/navigation.php'; ?>
     
+    <main>
+        <div class="showcase">
+            <h2>Free Crates</h2>
+            <?php
+                include 'includes/connect.php';
+
+                $query = 'SELECT * FROM crates WHERE price = 0 ORDER BY name ASC;';
+                $result = $conn->query($query);
+
+                if ($result->fetch_assoc() > 0) {
+                    foreach ($result as $r) {
+                        $status = isset($_SESSION['logged_in']) ? $_SESSION['logged_in'] ? true : false : false;
+                        $status_text = $status ? "Open" : "Login to open";
+                        echo '<div class="crate">
+                                <h3>' . $r['name'] . '</h3>
+                                <p>' . $r['description'] . '</p>
+                                <form action="view.php" method="get">
+                                    <input type="hidden" name="id" value="' . $r['id'] . '">
+                                    <input type="submit"'; if (!$status) echo ' disabled'; echo ' value="' . $status_text . '">
+                                </form>
+                            </div>';
+                    }
+                } else {
+                    echo "<p style='color: red;'>none</p>";
+                }
+            ?>
+        </div>
+    </main>
 </body>
 </html>
--- index.php
+++ index.php
@@ -11,5 +11,46 @@
 <body>
     <?php include 'includes/navigation.php'; ?>
     
+    <main>
+        <div class="showcase">
+            <h2>Main Crates</h2>
+            <?php
+                include 'includes/connect.php';
+                
+                $sgenerated = false;
+
+                $query = 'SELECT * FROM crates ORDER BY price ASC;';
+                $result = $conn->query($query);
+
+                if ($result->fetch_assoc() > 0) {
+                    $snames = ["Water Case", "Knife Case"];
+                    foreach ($result as $r) {
+                        $fprice = $r['price'];
+                        if ($fprice == 0) {
+                            $fprice = "Free";
+                            continue;
+                        } else {
+                            $fprice = $fprice . " vPLN";
+                        }
+                        if (in_array($r['name'], $snames) && $sgenerated == false) {
+                            echo "</div><div class='showcase'>
+                                    <h2>Special Crates</h2>";
+                            $sgenerated = true;
+                        }
+                        echo '<div class="crate">
+                                <h3>' . $r['name'] . '</h3>
+                                <p>' . $r['description'] . '</p>
+                                <form action="view.php" method="get">
+                                    <input type="hidden" name="id" value="' . $r['id'] . '">
+                                    <input type="submit" value="' . $fprice . '">
+                                </form>
+                            </div>';
+                    }
+                } else {
+                    echo "<p style='color: red;'>none</p>";
+                }
+            ?>
+        </div>
+    </main>
 </body>
 </html>
