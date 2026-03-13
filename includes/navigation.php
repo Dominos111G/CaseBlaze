bc4b27eef8e340bf0668097c9fc58da7e5c83a2d
Wojciech <w.majchrzak@zset.leszno.pl>
2026-03-13 13:22:09 +0100

    poprawka

--- includes/navigation.php
+++ includes/navigation.php
@@ -5,13 +5,13 @@
 
         <?php if (isset($_SESSION['user_id'])): ?>
             <li><a href="/wallet.php">Wallet</a></li>
-            <li><a href="/caseblaze/profile.php">Profile</a></li>
+            <li><a href="/profile.php">Profile</a></li>
             <li><?php if (isset($_SESSION['username'])) {echo $_SESSION['username'];} else {echo "User";} ?></li>
             <li><?php if (isset($_SESSION['wallet'])) {echo $_SESSION['wallet'] . " vPLN";} else {echo "0 vPLN";} ?></li>
             <li><a href="/includes/logout.php">Logout</a></li>
         <?php else: ?>
-            <li><a href="/caseblaze/login.php">Login</a></li>
-            <li><a href="/caseblaze/register.php">Register</a></li>
+            <li><a href="/login.php">Login</a></li>
+            <li><a href="/register.php">Register</a></li>
         <?php endif; ?>
     </ul>
 </nav>
