ed4ba170821b7940afb143edb6330bfad3ca308f
Wojciech <w.majchrzak@zset.leszno.pl>
2026-03-18 15:58:05 +0100

    cookie

--- wallet.php
+++ wallet.php
@@ -34,16 +34,22 @@ $user_id = $_SESSION['user_id'];
         </form>
         <?php
             if (isset($_POST['add'])) {
-            $amount = $_POST['add'];
+                $amount = $_POST['add'];
+                $cookie_name = "cooldown" . $amount;
+                if (isset($_COOKIE[$cookie_name])) {
+                    return;
+                }
 
-            $stmt = $conn->prepare("UPDATE users SET wallet = wallet + ? WHERE id = ?");
-            $stmt->bind_param("di", $amount, $user_id);
+                $stmt = $conn->prepare("UPDATE users SET wallet = wallet + ? WHERE id = ?");
+                $stmt->bind_param("di", $amount, $user_id);
 
-            if ($stmt->execute()) {
-                echo "Doładowano portfel!";
-            } else {
-                echo "Błąd!";
-            }
+                if ($stmt->execute()) {
+                    $cookie_value = true;
+                    setcookie($cookie_name, $cookie_value, time() + (60 * 5), "/"); 
+                    echo "Doładowano portfel!";
+                } else {
+                    echo "Błąd!";
+                }
             }
         ?>
     </div>
